9995de40e7cf11b78c5e130c8d3c26c08f35eac2
MD Habibulla <mdhabibulla146425@gmail.com>
2026-08-29 22:26:17 +0600

    new problem add

---
+++ p1005/average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+    float A,B;
+    scanf("%f %f",&A,&B);
+
+    double average=((A*3.5)+(B*7.5))/(3.5+7.5);
+
+    printf("MEDIA = %.5lf\n",average);
+    return 0;
+}
+//hello bro how are you
